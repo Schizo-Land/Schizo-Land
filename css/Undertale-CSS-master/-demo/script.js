d86ba0c8526776007a524cf0615b74afe3718db9
Schizo-Land <luxu.cubic@gmail.com>
2026-08-07 07:13:02 -0300

    Delete css directory

--- css/Undertale-CSS-master/-demo/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded",function() {
-	Undertale.presets.sprites.fight(document.getElementById("fight"));
-	Undertale.presets.sprites.save(document.getElementById("save"));
-	Undertale.presets.sprites.item(document.getElementById("item"));
-	Undertale.presets.sprites.mercy(document.getElementById("mercy"));
-});
